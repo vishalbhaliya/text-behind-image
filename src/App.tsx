3386c849b5815fa63e9e85fa4f60f3fb04102480
Vishal Bhaliya <vishalbhaliya94@gmail.com>
2024-11-18 14:14:17 +0530

    updates for routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/ui/navigation/nav-bar';
 import Home from './pages/Home';
 import TextBehindImage from './pages/TextBehindImage';
-import ImageEditor from './pages/ImageEditor';
 import About from './pages/About';
 
 function App() {
   return (
-    <Router basename='/'>
       <div className="min-h-screen bg-background">
         <NavBar />
         <main className="container mx-auto px-4 py-8">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route key={"text-behind-image"} path="/text-behind-image" element={<TextBehindImage />} />
-            <Route path="/image-editor" element={<ImageEditor />} />
-            <Route path="/about" element={<About />} />
+            <Route path="/" Component={Home} />
+            <Route key={"text-behind-image"} path="/text-behind-image" Component={TextBehindImage} />
+            <Route path="/about" Component={About} />
           </Routes>
         </main>
       </div>
-    </Router>
   );
 }
 
